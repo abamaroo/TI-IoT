ec0a9986f56240799b26517bad5e3deef17c4f7b
Abdullah Alawfi <asalawfi@outlook.com>
2022-03-08 13:16:38 -0500

    Serving the front end with django on local host

--- serverside/serverside/urls.py
+++ serverside/serverside/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.shortcuts import render
+
+def render_index(request):
+    return render(request, "index.html")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('superuser/', admin.site.urls),
+    re_path(r"^$", render_index),
+    re_path(r"^(?:.*)/?$", render_index),
 ]
